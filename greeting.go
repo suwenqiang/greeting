bf7dad6a3ee80726337b9c24f2e129c81fb2f00d
suwenqiang <suwenqiang2011@gmail.com>
2022-07-07 10:57:59 +0800

    add return error

--- greeting.go
+++ greeting.go
@@ -1,8 +1,12 @@
 package greeting
 
 import "fmt"
+import "errors"
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
