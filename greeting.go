af71740513ba1ea202fbbd4aca2b7755bf02c552
suwenqiang <suwenqiang2011@gmail.com>
2022-07-08 16:29:42 +0800

    update

--- greeting.go
+++ greeting.go
@@ -2,11 +2,26 @@ package greeting
 
 import "fmt"
 import "errors"
+import "math/rand"
+import "time"
 
 func Hello(name string) (string, error) {
 	if name == "" {
-		return "", errors.New("empty name")
+		return name, errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randFormate(), name)
 	return message, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randFormate() string {
+	formates := []string{
+		"Hi, %v. Welcome",
+		"Great to see you, %v",
+		"Hail, %v! Well met!",
+	}
+	return formates[rand.Intn(len(formates))]
 }
